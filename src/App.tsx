f047ef8d457a86458f44cb4b78ab045e43920575
eunsol <rungjinuna@naver.com>
2024-02-06 18:08:59 +0900

    style: 파일명 수정 및 import 경로수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "./pages/LoginPage/LoginPage"
 import WelcomePage from "./pages/WelcomePage/WelcomePage"
 import RegisterPage from "./pages/RegisterPage/RegisterPage"
-import ContainerPage from "./pages/ContainerPage/Container"
+import ContainerPage from "./pages/ContainerPage/ContainerPage"
 import IDEPage from "./pages/IDEPage/IDEPage"
 
 function App() {
