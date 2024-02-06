7be262a4dbdf39f48deaffbc59ac07b91f05ecce
eunsol <rungjinuna@naver.com>
2024-02-06 16:23:28 +0900

    feat: 진입페이지 _ welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "./pages/LoginPage/LoginPage"
 import IDE from "./pages/IDEPage/IDE"
 import WelcomePage from "./pages/WelcomePage/WelcomePage"
-import RegisterPage from "./pages/RegisterPage/Register"
+import RegisterPage from "./pages/RegisterPage/RegisterPage"
 import ContainerPage from "./pages/ContainerPage/Container"
 
 function App() {
