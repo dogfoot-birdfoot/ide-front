b75442f912dd95c639200eddcd398ec8e52da1a3
juwonk1018 <kjon1018@gmail.com>
2024-02-16 17:36:34 +0900

    design: tab의 유무에 따른 chatModal 위치 조정

--- src/components/modal/ChatModal.tsx
+++ src/components/modal/ChatModal.tsx
@@ -2,10 +2,18 @@ import React from "react"
 import ChatInputForm from "@/components/form/ChatInputForm"
 import Messages from "@/components/message/Messages"
 import ChatHeader from "@/components/header/ChatHeader"
+import { useActiveFile } from "context/ActiveFileContext"
 
 const ChatModal = () => {
+  const { tabs } = useActiveFile()
+
   return (
-    <div className={"absolute w-72 border border-blue-500 rounded-lg right-7 top-20 z-50 bg-white"}>
+    <div
+      className={
+        "absolute w-72 border border-blue-500 rounded-lg right-3 z-50 bg-white" +
+        (tabs.length === 0 ? " top-24" : " top-32")
+      }
+    >
       <ChatHeader></ChatHeader>
       <Messages></Messages>
       <ChatInputForm></ChatInputForm>
